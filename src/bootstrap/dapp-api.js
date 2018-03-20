e4012ba6baab83ca7930ca1594c3f3bc269ccf61
epiqueras <epiquerass@gmail.com>
2018-03-19 18:23:33 -0700

    chore: remove kleros specific env vars

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -2,8 +2,6 @@ import Eth from 'ethjs'
 
 const env = process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV'
 const ETHEREUM_PROVIDER = process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
-const STORE_PROVIDER = process.env[`REACT_APP_${env}_STORE_PROVIDER`]
-const ARBITRATOR_ADDRESS = process.env[`REACT_APP_${env}_ARBITRATOR_ADDRESS`]
 
 let eth
 if (process.env.NODE_ENV === 'test')
@@ -12,4 +10,4 @@ else if (window.web3 && window.web3.currentProvider)
   eth = new Eth(window.web3.currentProvider)
 else eth = new Eth.HttpProvider(ETHEREUM_PROVIDER)
 
-export { eth, ETHEREUM_PROVIDER, STORE_PROVIDER, ARBITRATOR_ADDRESS }
+export { eth, ETHEREUM_PROVIDER }
