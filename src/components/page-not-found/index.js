027049b6d71677f7d49a5050a394f346d09eb025
epiqueras <epiquerass@gmail.com>
2018-06-29 15:49:36 -0500

    refactor: remove unnecessary line

--- src/components/page-not-found/index.js
+++ src/components/page-not-found/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 import './page-not-found.css'
 
-const PageNotFound = () => (
+export default () => (
   <div className="PageNotFound">
     <div className="PageNotFound-message">
       404. This is not the page you are looking for.
     </div>
   </div>
 )
-
-export default PageNotFound
